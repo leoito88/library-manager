fff9fdf88f0bf346755a65f0d70111b2857a5175
Leandro Rojano <leandrorojano2001@hotmail.com>
2024-06-23 20:37:44 -0300

    Estructura inicial creada

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/* Gestión de una Biblioteca
+
+Estás a cargo de desarrollar un pequeño sistema de gestión para una biblioteca utilizando JavaScript. Este sistema debe permitir administrar libros y autores, y realizar algunas operaciones básicas sobre ellos.
+
+Para ello, debes crear un objeto Libro con las propiedades titulo (string), autor (string), añoPublicacion (número) y genero (string). Además, este objeto debe tener un método info que devuelva un string con la información del libro.
+
+
+Luego, crea un arreglo llamado biblioteca que contenga al menos tres objetos Libro con diferentes datos.
+
+A continuación, implementa una función buscarLibrosPorAutor que reciba como parámetro el nombre de un autor (string) y devuelva un arreglo con los libros escritos por ese autor.
+
+Finalmente, implementa una función añadirLibro que reciba como parámetros los datos de un nuevo libro (titulo, autor, añoPublicacion, genero) y lo añada al arreglo biblioteca.
+
+Tareas a realizar:
+
+    Define el objeto Libro con las propiedades y métodos mencionados.
+    Crea el arreglo biblioteca con al menos tres libros.
+    Implementa la función buscarLibrosPorAutor.
+    Implementa la función añadirLibro.
+    Prueba tu código añadiendo un nuevo libro a la biblioteca y luego buscando libros por un autor específico.
+
+Entrega:
+
+Sube tu proyecto a un repositorio en GitHub y comparte el enlace del repositorio. La fecha límite de entrega es el 24 de junio antes de las 9 am.
+
+Recuerda comentar */
