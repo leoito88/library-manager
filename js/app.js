437e09915fbdd6a4a6c21700a0e83b6eefbb0fd9
Leandro Rojano <leandrorojano2001@hotmail.com>
2024-06-23 20:45:22 -0300

    Se muestran los libros en el DOM

--- js/app.js
+++ js/app.js
@@ -24,3 +24,42 @@ Entrega:
 Sube tu proyecto a un repositorio en GitHub y comparte el enlace del repositorio. La fecha límite de entrega es el 24 de junio antes de las 9 am.
 
 Recuerda comentar */
+
+document.addEventListener("DOMContentLoaded", () =>{
+    buscarLibros()
+});
+
+// Parámetros Objeto Libro
+function Libro(titulo, autor, añoPublicacion, genero) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.añoPublicacion = añoPublicacion;
+    this.genero = genero;
+    this.info = function() {
+        return `- ${this.titulo} (Autor: ${this.autor}, Año: ${this.añoPublicacion}, Género: ${this.genero})`;
+    };
+}
+
+// Creamos algunos libros
+let libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967, "Realismo mágico");
+let libro2 = new Libro("Carrie", "Stephen King", 1974, "Novela de terror");
+let libro3 = new Libro("1984", "George Orwell", 1949, "Distopía");
+let libro4 = new Libro("Crónica de una muerte anunciada", "Gabriel García Márquez", 1981, "Realismo mágico");
+let libro5 = new Libro("El amor en los tiempos del cólera", "Gabriel García Márquez", 1985, "Realismo mágico");
+let libro6 = new Libro("The Shining", "Stephen King", 1977, "Novela de terror");
+let libro7 = new Libro("Harry Potter and the Philosopher’s Stone", "J.K. Rowling", 1997, "Literatura fantástica");
+let libro8 = new Libro("It", "Stephen King", 1986, "Novela de terror");
+let libro9 = new Libro("Harry Potter and the Chamber of Secrets", "J.K. Rowling", 1998, "Literatura fantástica");
+
+
+// Agregamos los libros a la biblioteca
+let biblioteca = [libro1, libro2, libro3, libro4, libro5, libro6, libro7, libro8, libro9];
+
+// Mostramos los libros en la página
+let bibliotecaDiv = document.getElementById("biblioteca");
+biblioteca.forEach(libro => {
+    let libroDiv = document.createElement("div");
+    libroDiv.className = "libro";
+    libroDiv.textContent = libro.info();
+    bibliotecaDiv.appendChild(libroDiv);
+});
